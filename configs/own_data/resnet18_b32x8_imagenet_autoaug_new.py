0fc781fabe9e69a5c1799f7b777ac2875be76c7a
Taesiny <32998825+Taesiny@users.noreply.github.com>
2022-03-15 16:40:37 +0100

    change epochs

--- configs/own_data/resnet18_b32x8_imagenet_autoaug_new.py
+++ configs/own_data/resnet18_b32x8_imagenet_autoaug_new.py
@@ -2,4 +2,4 @@ _base_ = [
     '../_base_/models/resnet18_pre.py', '../_base_/datasets/own_data_new.py',
     '../_base_/schedules/imagenet_bs256.py', '../_base_/default_runtime.py'
 ]
-runner = dict(type='EpochBasedRunner', max_epochs=500)
+runner = dict(type='EpochBasedRunner', max_epochs=300)
